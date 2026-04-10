196a746e0dc773b9fd6110f1517fe1197982ef07
zhixiaosu@gmail.com <sue.su@alleninstitute.org>
2026-04-10 22:22:43 +0000

    save in result

--- code/beh_ephys_analysis/utils/capsule_migration.py
+++ code/beh_ephys_analysis/utils/capsule_migration.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 def capsule_directories():
-    output_dir = Path('/root/capsule/scratch/results')
+    output_dir = Path('/root/capsule/results')
     dirs = {
         'output_dir': output_dir,
         'manuscript_fig_dir': output_dir / 'manuscript' / 'figures',
         'manuscript_fig_prep_dir': output_dir / 'manuscript' / 'prep',
-        'derived_dir': Path('/root/capsule/scratch'),
+        'derived_dir': Path('/root/capsule/data/scratch_data'),
         'data_dir': Path('/root/capsule/data'),
     }
     for dir in dirs.values():
